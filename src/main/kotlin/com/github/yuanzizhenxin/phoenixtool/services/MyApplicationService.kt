9a42b88d2615600ff5f6761f884d581e71a993ba
GitHub Action <action@github.com>
2022-07-06 12:05:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/yuanzizhenxin/phoenixtool/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/yuanzizhenxin/phoenixtool/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.yuanzizhenxin.phoenixtool.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yuanzizhenxin.phoenixtool.MyBundle
 
 class MyApplicationService {
 
